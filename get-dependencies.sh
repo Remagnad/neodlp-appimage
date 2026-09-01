2106a31831e60a877255721906196aad900475b5
Remagnad <dl99236@gmail.com>
2026-09-01 00:43:05 -0300

    Modify dependency installation in get-dependencies.sh
    Updated package installation list and specified AUR package.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,14 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+ pacman -Syu --noconfirm \
+     ffmpeg              \
+     aria2               \
+     libappindicator3-1  \
+     libwebkit2gtk-4.1-0 \
+     libgtk-3-0          \
+     
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package neodlp
 
 # If the application needs to be manually built that has to be done down here
 
