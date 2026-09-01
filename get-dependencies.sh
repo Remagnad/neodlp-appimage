751d53b7499d47d4cdb1f6c1da448e22c71b736e
Remagnad <dl99236@gmail.com>
2026-09-01 01:13:33 -0300

    Refactor package installation in get-dependencies.sh
    Updated package installation commands in get-dependencies.sh.

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -9,7 +9,8 @@ echo "---------------------------------------------------------------"
  pacman -Syu --noconfirm \
      ffmpeg              \
      aria2               \
-     libayatana-appindicator \
+     libappindicator \
+     webkit2gtk-4.1 \
      
 
 echo "Installing debloated packages..."
@@ -17,7 +18,10 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package neodlp
+make-aur-package \
+ libsoup \
+ neodlp \
+ 
 
 # If the application needs to be manually built that has to be done down here
 
